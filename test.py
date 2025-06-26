095ba8e1f9797200b85b5c4c4b66750a28faade1
Nam Pho <npho@uw.edu>
2025-06-26 07:53:37 -0700

    test scripts

---
+++ test.py
@@ -0,0 +1,4 @@
+from isaacsim import SimulationApp
+
+simulation_app = SimulationApp({"headless": True})
+
---
+++ test.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+source /opt/python/isaac/.venv/bin/activate
+
+python /opt/test.py
+
